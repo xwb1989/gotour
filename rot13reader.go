8d3e914217694207fc4c1712d5ef4533545eb9bd
Wenbin Xiao <xwb1989@gmail.com>
2014-01-22 13:01:41 -0500

    rot13reader.go

---
+++ rot13reader.go
@@ -0,0 +1,37 @@
+//File_name: slices.go
+//Author: Wenbin Xiao
+//Description: http://tour.golang.org/#63
+
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r *rot13Reader) Read(p []byte) (n int, err error) {
+	size, _ := r.r.Read(p)
+	for i, v := range p[:size] {
+		if (v > byte('z') || v < byte('a')) && (v > byte('Z') || v < byte('A')) {
+			continue
+		}
+		if v+13 > byte('z') {
+			p[i] = v - 13
+		} else {
+			p[i] = v + 13
+		}
+	}
+	return size, nil
+}
+
+func main() {
+	s := strings.NewReader(
+		"Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
